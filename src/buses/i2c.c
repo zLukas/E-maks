6f72f3eff13a892298a1a6bf54761ff8ddb2a0df
Lukas <xlukas9@gmail.com>
2018-10-16 16:32:57 +0200

    i2c update

--- src/buses/i2c.c
+++ src/buses/i2c.c
@@ -3,6 +3,19 @@
 
 I2C_InitTypeDef i2c;
 
+uint8_t findAddress(uint32_t deviceAddress)
+{
+	uint8_t addressSet = 0;
+	uint32_t addressReceived;
+	i2cReveive(DEVICE_MEMORY_ADDRESS,&addressReceived, sizeof(addressReceived));
+	if ( addressReceived== deviceAddress)
+		{ addressSet = 1;}
+	else
+	{addressSet = 0;}
+	
+	return addressSet;
+}
+
 void i2cInit(void)
 {
 	I2C_StructInit(&i2c);
@@ -10,50 +23,59 @@ void i2cInit(void)
 	i2c.I2C_ClockSpeed = 100000;
 	I2C_Init(I2C1, &i2c);
 	I2C_Cmd(I2C1, ENABLE);
-	
-
 } 
 
-
-void i2cSend(int deviceAddress ,int dataToSend)
+void setDeviceMemeoryAddress(uint32_t DeviceMemoryAddress)
 {
-	 I2C_GenerateSTART(I2C1, ENABLE);
-	 while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS);
-	
-	I2C_Send7bitAddress(I2C1, deviceAddress, I2C_Direction_Transmitter);
-	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) != SUCCESS);
- 
-	/*
-	I2C_SendData(I2C1, stm32address??);
-  while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
-	*/
-	
-	I2C_SendData(I2C1, dataToSend);
-  while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
-	
-	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
-  I2C_GenerateSTOP(I2C1, ENABLE);
-
-}	
-
-
-int i2cReveive(void)
-{
-	int data;
 	I2C_GenerateSTART(I2C1, ENABLE);
 	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS);
-	I2C_AcknowledgeConfig(I2C1, ENABLE);
-	
-	I2C_Send7bitAddress(I2C1, 0xa0, I2C_Direction_Receiver);
-	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS);
+	I2C_Send7bitAddress(I2C1, DEVICE_ADDRESS, I2C_Direction_Transmitter);
+	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) != SUCCESS);
+ 
+	I2C_SendData(I2C1, DeviceMemoryAddress);
+	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
+}
 	
 
-	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS);
-	uint8_t data1 = I2C_ReceiveData(I2C1);
+
+void i2cSend(uint32_t deviceMemoryAddress , const void* data, int size)
+{
+	  int i;
+ const uint8_t* buffer = (uint8_t*)data;
+ 
+ setDeviceMemeoryAddress(deviceMemoryAddress);
+ for (i = 0; i < size; i++) {
+ I2C_SendData(I2C1, buffer[i]);
+ while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING) != SUCCESS);
+ }
+ I2C_GenerateSTOP(I2C1, ENABLE);
 	
-	I2C_AcknowledgeConfig(I2C1, DISABLE);
-  I2C_GenerateSTOP(I2C1, ENABLE);
-  while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS);
-  data = I2C_ReceiveData(I2C1);
-	return data;
+}	
+
+// do konfiguracji
+int i2cReveive( uint32_t deviceMemoryAddress , const void* data, int size)
+{
+	 int i;
+	uint8_t* buffer = (uint8_t*)data;
+ 
+	setDeviceMemeoryAddress(DEVICE_ADDRESS);
+ 
+	I2C_GenerateSTART(I2C1, ENABLE);
+	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS);
+ 
+	I2C_AcknowledgeConfig(I2C1, ENABLE);
+	I2C_Send7bitAddress(I2C1, DEVICE_ADDRESS, I2C_Direction_Receiver);
+	while (I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS);
+ 
+	for (i = 0; i < size - 1; i++) 
+	{
+		 while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS);
+     buffer[i] = I2C_ReceiveData(I2C1);
+	}
+ I2C_AcknowledgeConfig(I2C1, DISABLE);
+    I2C_GenerateSTOP(I2C1, ENABLE);
+    while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS);
+    buffer[i] = I2C_ReceiveData(I2C1);
+	return (int)buffer;
+	
 }
--- src/buses/i2c.h
+++ src/buses/i2c.h
@@ -5,7 +5,12 @@
 #include <hardware.h>
 #include <stm32f10x_i2c.h>
 
-#define DEVICE_ADDRESS  
+#define DEVICE_ADDRESS 				  0x00 // ustawic poprawny addres urzadzenia
+#define DEVICE_MEMORY_ADDRESS	0x00//ustwic poprawny dla urzadzenia
 
- 
+void i2cInit(void);
+uint8_t findAddress(uint32_t deviceAddress);
+void setDeviceMemoryAddress(uint32_t DeviceMemoryAddress);
+void i2cSend(uint32_t deviceMemoryAddress , const void* data, int size);
+int i2cReveive( uint32_t deviceMemoryAddress , const void* data, int size);
 #endif
